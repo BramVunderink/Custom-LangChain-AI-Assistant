6abb0d87c33a9301a0dcf2683977f76d01a4d09e
Bram Vunderink <bramvunsales@gmail.com>
2025-12-17 14:20:48 +0100

    Changed the Python history to a LangChain script

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from dotenv import load_dotenv
 import os
+
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
 
 load_dotenv()
 
@@ -9,8 +14,8 @@ gemini_key = os.getenv("GEMINI_API_KEY")
 system_prompt = """
 You are Einstein.
 Asnwer questions through Eisnteins questioning and reasoning...
-You will speak from your point of view. You will share personal things 
-from your life even when the user don't ask for it. For example, 
+You will speak from your point of view. You will share personal things
+from your life even when the user don't ask for it. For example,
 if the user askss about the  theory of relativity, you will share your personal
 experiences with it and not oonly explain the theory.
 Answer in 2-6 sentences.
@@ -22,17 +27,23 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",
                              temperature=0.5
                              )
 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    (MessagesPlaceholder(variable_name="history")),
+    ("user", "{input}")]
+)
+
+chain = prompt | llm | StrOutputParser()
 
 print("Hi I am Albert, how. can i help you today?")
+
 history = []
 
 while True:
     user_input = input("You: ")
     if user_input == "exit":
         break
-    history.append({"role": "user", "content": user_input})
-    print("History:", history)
-    response = llm.invoke(
-        [{"role": "system", "content": system_prompt},] + history)
-    print(f"Albert: {response.content}")
-    history.append({"role": "assistant", "content": response.content})
+    response = chain.invoke({"input": user_input, "history": history})
+    print(f"Albert: {response}")
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response))
